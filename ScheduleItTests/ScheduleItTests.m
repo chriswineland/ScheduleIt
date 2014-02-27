1b74d0f4bac42fd2fef000f581c181fbef053170
Chris Wineland <chriswineland01@gmail.com>
2014-02-26 17:18:55 -0800

    Initial Commit

---
+++ ScheduleItTests/ScheduleItTests.m
@@ -0,0 +1,34 @@
+//
+//  ScheduleItTests.m
+//  ScheduleItTests
+//
+//  Created by Chris Wineland on 2/26/14.
+//  Copyright (c) 2014 Chris Wineland. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScheduleItTests : XCTestCase
+
+@end
+
+@implementation ScheduleItTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
