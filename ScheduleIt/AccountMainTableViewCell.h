0f76bbe23d0353bc7e63c6fc2d83a8d819a191b7
CWineland <winealnd@seawolf.sonoma.edu>
2014-03-07 12:41:23 -0800

    added cell for accounts main table view

---
+++ ScheduleIt/AccountMainTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  AccountMainTableViewCell.h
+//  ScheduleIt
+//
+//  Created by Chris Wineland on 3/7/14.
+//  Copyright (c) 2014 Chris Wineland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountMainTableViewCell : UITableViewCell
+
+@end
---
+++ ScheduleIt/AccountMainTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  AccountMainTableViewCell.m
+//  ScheduleIt
+//
+//  Created by Chris Wineland on 3/7/14.
+//  Copyright (c) 2014 Chris Wineland. All rights reserved.
+//
+
+#import "AccountMainTableViewCell.h"
+
+@implementation AccountMainTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
